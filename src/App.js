657a55ec1931d11f8b396c4645c0b41363285034
vishal <vishalgehlot334@gmail.com>
2022-08-17 20:38:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { app } from "./Compos/Firebase";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Box,
+  HStack,
+  Container,
+  VStack,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import Message from "./Compos/Message";
+import {
+  onAuthStateChanged,
+  signOut,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  serverTimestamp,
+} from "firebase/firestore";
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const logoutHandler = () => signOut(auth);
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+};
 
 function App() {
+  
+  const [user, setuser] = useState(false);
+  const [msg, setmsg] = useState("");
+  const [masgs, setmasgs] = useState([]);
+  const divForScroll = useRef(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setmsg(" ");
+    try {
+      await addDoc(collection(db, "msgs"), {
+        text: msg,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp()
+      });
+     
+      divForScroll.current.scrollIntoView({ behaviour: "smooth" });
+    } catch (err) {
+      alert(err);
+    }
+  };
+  useEffect(() => { 
+    const q =query(collection(db,"msgs"),orderBy("createdAt","asc"))
+    const unsub = onAuthStateChanged(auth, (data) => {
+      setuser(data);
+    });
+const unsubmsg=onSnapshot(q,(snap)=>{
+  setmasgs(snap.docs.map((item)=>{
+    const id=item.id;
+    return{id, ...item.data()}
+  
+  }))
+})
+
+    return () => {
+      unsub();
+      unsubmsg();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box bg={"red.100"}>
+        {user ? (
+          <Container h={"100vh"} bg={"white"}>
+            <VStack padding={"4"} h="full">
+              <Button onClick={logoutHandler} w={"full"} colorScheme={"red"}>
+                logout
+              </Button>
+
+              <VStack
+                css={{ "&::-webkit-scrollbar": { display: "none" } }}
+                overflowY="auto"
+                h={"full"}
+                w={"full"}
+              >
+                {masgs.map((item) => (
+                  <Message
+                  text={item.text}
+                    key={item.id}
+                    user={item.uid === user.uid ? "me" : "other"}
+                    uri={item.uri}
+                  />
+                ))}
+                <div ref={divForScroll}></div>
+              </VStack>
+
+              <form
+                onSubmit={submitHandler}
+                
+                style={{ width: "100%" }}
+              >
+                <HStack>
+                  <Input value={msg}   onChange={(e) => setmsg(e.target.value)} placeholder="Enter message" />
+                  <Button colorScheme={"blue"} type="submit">
+                    Send
+                  </Button>
+                </HStack>
+              </form>
+            </VStack>
+          </Container>
+        ) : (
+          <VStack justifyContent={"center"} bg={"white"} h={"100vh"}>
+            <Button onClick={loginHandler} colorScheme={"yellow"}>
+Sign In with Google
+            </Button>
+          </VStack>
+        )}
+      </Box>
+    </>
   );
 }
 
---
+++ src/Compos/Firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsq_xliutwKCgYsPeYaIrJ6lYrQV__kNg",
+  authDomain: "vishchat-3582b.firebaseapp.com",
+  projectId: "vishchat-3582b",
+  storageBucket: "vishchat-3582b.appspot.com",
+  messagingSenderId: "619928779337",
+  appId: "1:619928779337:web:19a356cde90f531947dd98"
+};
+
+export const app = initializeApp(firebaseConfig);
